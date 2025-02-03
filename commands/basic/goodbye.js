1dea7b8cb9d56868312e49cff0a06cbec029ebee
= <=>
2025-02-03 14:23:55 -0600

    random hello/goodbye

--- commands/basic/goodbye.js
+++ commands/basic/goodbye.js
@@ -2,6 +2,16 @@ module.exports = {
     name: "goodbye",
     description: "Does the basic bot goodbye.",
     async execute(message) {
-        message.reply("See you next time!");
+        const farewells = [
+            "Goodbye!", "See you later!", "Farewell!", "Take care!", "Bye-bye!", 
+            "Adios!", "Ciao!", "Au revoir!", "Sayonara!", "Auf Wiedersehen!", 
+            "See you next time!", "Catch you later!", "So long!", "Toodles!", "Later, alligator!", 
+            "Peace out!", "Smell ya later!", "Stay safe!", "Until next time!", "Have a great day!", 
+            "Bon voyage!", "Cheerio!", "Godspeed!", "Parting is such sweet sorrow!", "See ya!", 
+            "Be well!", "Keep in touch!", "Stay awesome!", "May we meet again!", "Don't be a stranger!", "I will not miss you."
+        ];
+
+        const randomFarewell = farewells[Math.floor(Math.random() * farewells.length)];
+        message.reply(randomFarewell);
     }
 };
--- commands/basic/hello.js
+++ commands/basic/hello.js
@@ -2,6 +2,16 @@ module.exports = {
     name: "hello",
     description: "Does the basic bot greeting.",
     async execute(message) {
-        message.reply("Hello! ParkerBot is armed and ready.");
+        const greetings = [
+            "Hello!", "Hi!", "Hey!", "Greetings!", "Salutations!", 
+            "Howdy!", "Yo!", "What's up?", "Good day!", "Bonjour!", 
+            "Hola!", "Ciao!", "Hallo!", "Olá!", "Namaste!", 
+            "Salam!", "Konnichiwa!", "Annyeong!", "Zdravstvuyte!", "Shalom!", 
+            "Sawubona!", "Merhaba!", "Yassas!", "Hej!", "Hei!", 
+            "Hallå!", "Aloha!", "Ahoy!", "Szia!", "Mabuhay!", "Existance is pain."
+          ];
+
+        const randGreeting = greetings[Math.floor(Math.random() * greetings.length)];
+        message.reply(randGreeting);
     }
 };
