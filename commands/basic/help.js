c3fb6730b36a197a689681acbea7217a485d2999
= <=>
2025-04-26 19:40:19 -0500

    small changes

--- commands/basic/help.js
+++ commands/basic/help.js
@@ -2,13 +2,10 @@ module.exports = {
     name: "help",
     description: "Lists out commands.",
     async execute(message) {
-        commands = [
-            'help',
-            'hello',
-            'goodbye',
-            'userinfo (@username)',
-        ]
-        const commandList = commands.map(cmd => `* !${cmd}`).join("\n")
-        message.reply("```Commands:\n" + commandList + "```");
+        const commandList = Array.from(message.client.commands.values())
+            .map(cmd => `* !${cmd.name} - ${cmd.description}`)
+            .join("\n");
+
+        message.reply(`Commands:\n${commandList}`);
     }
 };
